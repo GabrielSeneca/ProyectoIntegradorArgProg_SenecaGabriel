9c6f82504fcb3dd86cddcd3685c6d421c4421b3e
Gabriel Seneca <86847957+GabrielSeneca@users.noreply.github.com>
2022-05-20 17:28:16 -0300

    add backend con netbeans

---
+++ ProyectoIntegradorArgProg_SenecaGabriel/BackEnd/src/test/java/com/portfolio/GHS/GhsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portfolio.GHS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GhsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
